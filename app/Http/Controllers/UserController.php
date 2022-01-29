7cdb4ac548f1ad27751764d9c81d1c9fdbef26d1
= <=>
2022-01-28 21:10:02 -0300

    k c t

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,13 +17,17 @@ class UserController extends Controller
         $num1 = $request->num1;
         $num2 = $request->num2;
         logger()->debug('Sub feita', ['num1' => $num1, 'num2' => $num2, 'sub' => $sub]);
+        return $num1 - $num2;
     }
-    public function div(Type $var = null)
+    public function div(Request $request)
     {
         $num1 = $request->num1;
         $num2 = $request->num2;
         if ($num2==0){
-        logger()->error('Divisor zero!');
+            logger()->error('Divisor zero!');
+            return false;
         }
+        return $num1 / $num2;
+
     }
 }
